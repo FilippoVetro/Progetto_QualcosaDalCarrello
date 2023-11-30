58e02e08f8744c7ae501d3bf4f636905b65b8db6
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-11-30 12:09:22 +0100

    [ADD] setupped web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ProductsModule } from './products.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './services/data.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ProductsModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/types/hero.model.ts
@@ -0,0 +1,5 @@
+// hero.model.ts
+export interface Hero {
+  id: number;
+  name: string;
+}
