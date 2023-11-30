b1c0d426a823e337aa3095ee1f2ad11c1a3455d3
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-11-30 14:49:00 +0100

    [ADD] Angular Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ProductsModule } from './products.module';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { DataService } from './services/data.service';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     ProductsModule,
+    MaterialModule,
     HttpClientInMemoryWebApiModule.forRoot(DataService),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [MatSlideToggleModule, MatSortModule],
+})
+export class MaterialModule {}
