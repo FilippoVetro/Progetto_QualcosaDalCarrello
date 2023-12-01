b9543ccffd1b030728fc5ad0a4af3c63918651c4
abFilippo <Filippo.vetro.3d@gmail.com>
2023-12-01 12:05:36 +0100

    changes component name

---
+++ src/app/components/product-detail-modal/product-detail-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-modal',
+  templateUrl: './product-detail-modal.component.html',
+  styleUrls: ['./product-detail-modal.component.css']
+})
+export class ProductDetailCardComponent {
+
+}
