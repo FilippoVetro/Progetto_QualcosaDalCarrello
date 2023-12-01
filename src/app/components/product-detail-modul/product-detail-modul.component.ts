e0880d5d63ceffb3e9f7cf298ebe1993555798bc
abFilippo <Filippo.vetro.3d@gmail.com>
2023-12-01 12:07:04 +0100

    add product-deatil-modul

---
+++ src/app/components/product-detail-modul/product-detail-modul.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-modul',
+  templateUrl: './product-detail-modul.component.html',
+  styleUrls: ['./product-detail-modul.component.css']
+})
+export class ProductDetailModulComponent {
+
+}
