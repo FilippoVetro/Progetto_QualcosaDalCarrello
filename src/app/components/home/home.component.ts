eafd641f497f324f717eb5fbf8a2d954258f7081
Matteo Castellani <caste130804@gmai.com>
2023-12-04 10:22:44 +0100

    aa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+  // Dati di esempio per il carosello dei prodotti
+  bestSellingProducts: string[] = ['Desktop', 'Laptop', 'Software', 'Hardware', 'Tablet', 'Accessories'];
+
+  // Dati di esempio per il grafico di affidabilità e percentuali di vendita
+  reliabilityData = {
+    totalProductsSold: 1000,
+    complaintPercentage: 5,
+    supportResponseTime: '24 hours'
+   
+  };
+
+  categorySalesPercentages = {
+    desktop: 25,
+    laptop: 20,
+    software: 15,
+    hardware: 10,
+    tablet: 15,
+    accessories: 15
+  };
 }
