d552a75fac861f97659e4271e1235bf40e44190e
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-13 11:39:04 +0100

    [REF] Disabled Deactivate Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
   {
     path: 'orders',
     component: OrdersComponent,
-    canDeactivate: [canDeactivateGuard],
+    // canDeactivate: [canDeactivateGuard],
   },
   {
     path: 'support',
